88a5b0c5894548d959d4b63eba7e46f29d24bc85
RussoSk <117772437+RussoSk@users.noreply.github.com>
2023-05-07 23:16:38 +0300

    1.3
    task1,2,3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,5 @@
 import Vimeo from '@vimeo/player';
-import throttle from 'lodash/throttle'
+import throttle from 'lodash/throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import throttle from 'lodash/throttle';
 
+const FB_LOKALSTORAGE_KAY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+
+
+// збереження даних форми у локальне сховище з затримкою 500
+const saveFormLocalStorage = throttle(() => {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(FB_LOKALSTORAGE_KAY, JSON.stringify(formState));
+}, 500);
+
+// введення  даних  в форму
+form.addEventListener('input', saveFormLocalStorage);
+
+//  відновлення даних форми з локального сховища
+const restoreFormFromLocalStorage = () => {
+  const savedState = localStorage.getItem(FB_LOKALSTORAGE_KAY);
+  if (savedState) {
+    const formState = JSON.parse(savedState);
+    emailInput.value = formState.email;
+    messageInput.value = formState.message;
+  }
+};
+
+// відновлення даних форми при завантаженні сторінки
+restoreFormFromLocalStorage();
+
+
+
+// сабміт, очищення
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(formState);
+  
+//   emailInput.value = '';
+//   messageInput.value = '';
+  form.reset();
+  localStorage.removeItem(FB_LOKALSTORAGE_KAY);
+});
